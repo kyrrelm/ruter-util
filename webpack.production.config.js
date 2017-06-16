51e900f58c3cb6ed7f8203f40dc47b49d24ea02b
kyrremoe <kyrre.moe@bekk.no>
2017-06-17 01:12:43 +0200

    Skeleton setup for React with yarn, webpack, webpack-dev-server and webpack production build setup

---
+++ webpack.production.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './client/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: path.resolve('dist'),
+    filename: 'min.bundle.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
+    ]
+  },
+
+  plugins: [
+      HtmlWebpackPluginConfig,
+      new webpack.optimize.UglifyJsPlugin(),
+      new webpack.optimize.AggressiveMergingPlugin()
+  ]
+};
