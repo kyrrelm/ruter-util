3100a6cae781153455b519d3040851df086548fb
kyrremoe <kyrre.moe@bekk.no>
2017-06-20 23:19:20 +0200

    Autoupdate av tider

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Kort from './app/Kort.jsx';
+import  moment from 'moment';
 
 export default class App extends React.Component {
   constructor() {
@@ -7,31 +8,32 @@ export default class App extends React.Component {
     this.state = {
       avganger: [],
     };
-    fetch("http://reisapi.ruter.no/StopVisit/GetDepartures/3011450?datetime=2017-06-18T15:00")
+    //fetch("http://reisapi.ruter.no/StopVisit/GetDepartures/3011450?datetime=2017-06-18T15:00")
+    fetch("http://reisapi.ruter.no/StopVisit/GetDepartures/3011450")
         .then((res) => res.json())
         .then((body) => body.map((avgang) => {return {
           line: avgang.MonitoredVehicleJourney.PublishedLineName,
           destination: avgang.MonitoredVehicleJourney.MonitoredCall.DestinationDisplay,
-          departureTime: avgang.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime,
+          departureTime: moment(avgang.MonitoredVehicleJourney.MonitoredCall.AimedDepartureTime),
         }})).then((avganger) => this.setState({avganger: avganger}));
   }
 
+
+
+  componentDidMount() {
+
+  }
+
   render() {
-    console.log("avganger" , this.state.avganger);
+    console.log("time" , moment());
     const avgangskort = this.state.avganger.map(avgang =>
         <Kort key={avgang.departureTime} line={avgang.line} destination={avgang.destination} departureTime={avgang.departureTime}/>
     );
-    console.log("avgang" , avgangskort);
     return (
-      <div>
-        <div style={{textAlign: 'center'}}>
-          <h1>Hello World</h1>
-        </div>
-        <div className="kortliste">
-          <ul>
-            {avgangskort}
-          </ul>
-        </div>
+      <div className="kortliste">
+        <ul>
+          {avgangskort}
+        </ul>
       </div>
     );
   }
--- client/app/Kort.jsx
+++ client/app/Kort.jsx
@@ -1,17 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
-const Kort = ({line, destination, departureTime}) => {
-  console.log(line);
-  return (
+class Kort extends React.Component {
+  componentDidMount() {
+    setInterval(() => this.forceUpdate() , 10000);
+  }
+
+  render() {
+    const departureTime = this.props.departureTime;
+    const diff = departureTime.diff(moment(), 'seconds');
+    let displayTime = departureTime.fromNow();
+    if(30 >= diff && diff >= -30){
+      displayTime = 'now';
+    }
+
+    return (
       <li className = "kort">
         <div className = "kort-innhold">
-          <h2><b>{line + ' ' + destination}</b></h2>
-          <p>{departureTime}</p>
+          <h2><b>{this.props.line + ' ' + this.props.destination}</b></h2>
+          <p>{displayTime}</p>
         </div>
       </li>
-  );
-};
+    )
+  }
+}
 
 Kort.propsTypes = {
   departureTime: PropTypes.string.isRequired,
