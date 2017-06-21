7c53d14552e3e259660504850280bde1b85855ee
kyrremoe <kyrre.moe@bekk.no>
2017-06-22 00:07:12 +0200

    Fixed build script

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -21,7 +21,7 @@ module.exports = {
     loaders: [
       { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
       { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: ExtractTextPlugin.extract("style-loader", "css-loader") }
+      { test: /\.css$/, loader: ExtractTextPlugin.extract({ fallback: 'style-loader', use: 'css-loader' }) }
 
     ]
   },
