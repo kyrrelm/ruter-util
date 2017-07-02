29aaa64a9ca931afcec8b50909c8e71a4dda11ee
kyrremoe <kyrre.moe@bekk.no>
2017-07-02 16:33:34 +0200

    changed to hashRouter

--- client/app/App.jsx
+++ client/app/App.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { HashRouter, Route } from 'react-router-dom';
 
 import AppLayout from './AppLayout.jsx';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Router>
+      <HashRouter>
         <Route path="/" component={AppLayout}/>
-      </Router>
+      </HashRouter>
     );
   }
 }
